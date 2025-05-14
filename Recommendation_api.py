d940a4e0626e9d2b72c75da626ff8cbd37345c80
MariamEslam01 <165386015+MariamEslam01@users.noreply.github.com>
2025-05-14 10:19:21 +0400

    Update Recommendation_api.py

--- Recommendation_api.py
+++ Recommendation_api.py
@@ -221,5 +221,5 @@ def get_details():
 
 # Run the app
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000, debug=True)
+    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
 
